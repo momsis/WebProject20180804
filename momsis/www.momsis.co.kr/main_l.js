b79abc4bb66d308c833d8b4ae8d57c26a0d893a1
momsis <42083501+momsis@users.noreply.github.com>
2018-08-04 15:59:11 +0900

    test

---
+++ momsis/www.momsis.co.kr/main_l.js
@@ -0,0 +1,7 @@
+document.writeln('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="200" height="80">');
+document.writeln('<param name="movie" value="/flash/main_l.swf">');
+document.writeln('<param name="quality" value="high">');
+document.writeln('<param name="wmode" value="transparent">');
+document.writeln('<embed src="/flash/main_l.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="200" height="804">');
+document.writeln('</embed>');
+document.writeln('</object>');
